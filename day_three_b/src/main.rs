867eac5ae4771e54cbd3c4c3925eef220a63eeeb
Joshuah Boardman <joshuahboardman@gmail.com>
2023-08-13 14:23:53 -0500

    Update: finished day three part b

--- day_three_b/src/main.rs
+++ day_three_b/src/main.rs
@@ -1,24 +1,5 @@
 use std::collections::HashSet;
 
-// TODO:
-// Split the lines into 3 line groups
-// Check all three lines to find the shared item.
-
-// IDEA: Could make each line a hashset and then iterate one and check if the other two have the
-// same value.
-/*fn find_matching_item(first_pocket: &str, second_pocket: &str, &str) -> char {
-    let mut result = None;
-    let set: HashSet<char> = first_pocket.into_iter().collect();
-
-    for item in second_pocket {
-        if set.contains(&item) {
-            result = Some(item);
-            break;
-        }
-    }
-    result.unwrap()
-}*/
-
 fn get_item_priority(item: char) -> u8 {
     let mut item_value: u8 = item as u8;
 
@@ -30,6 +11,17 @@ fn get_item_priority(item: char) -> u8 {
     item_value
 }
 
+fn check_for_badges(ruck_sacks: &[HashSet<char>]) -> char {
+    let mut badge = None;
+    for item in ruck_sacks[0].iter() {
+        if ruck_sacks[1].get(item).is_some() && ruck_sacks[2].get(item).is_some() {
+            badge = Some(item);
+            break;
+        }
+    }
+    *badge.unwrap()
+}
+
 fn main() {
     let rucksacks = include_str!("../input.txt")
         .lines()
@@ -38,21 +30,12 @@ fn main() {
 
     let rucksack_groups = rucksacks.chunks(3);
 
-    //TODO: iterate over one of the hashsets per gourp and see if the other two contain the same
-    //values.
-
-}
-
-/*fn main() {
-    let item_sum: u32 = include_str!("../input.txt")
-        .lines()
-        .map(|line| {
-            let (first_pocket, second_pocket) = line.split_at(line.len() / 2);
-            let first_pocket: Vec<char> = first_pocket.chars().collect();
-            let second_pocket: Vec<char> = second_pocket.chars().collect();
-            let item = find_matching_item(first_pocket, second_pocket);
-            get_item_priority(item) as u32
+    let priority_sum: u32 = rucksack_groups
+        .map(|group| {
+            let badge = check_for_badges(group);
+            get_item_priority(badge) as u32
         })
         .sum();
-    println!("{}", item_sum);
-}*/
+
+    println!("{priority_sum:?}");
+}
