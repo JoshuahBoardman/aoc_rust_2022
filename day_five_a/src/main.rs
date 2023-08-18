dd894d8048828ce0be3aa0b71660ee990cac98f0
Joshuah Boardman <joshuahboardman@gmail.com>
2023-08-18 12:49:03 -0500

    Update: finished day five part a

--- day_five_a/src/main.rs
+++ day_five_a/src/main.rs
@@ -11,13 +11,13 @@ use std::str;
 
 #[derive(Debug)]
 struct Instruction {
-    quantity: u32,
-    start: u32,
-    end: u32,
+    quantity: i32,
+    start: i32,
+    end: i32,
 }
 
 impl Instruction {
-    fn new(quantity: u32, start: u32, end: u32) -> Self {
+    fn new(quantity: i32, start: i32, end: i32) -> Self {
         Instruction {
             quantity: quantity,
             start: start,
@@ -28,12 +28,12 @@ impl Instruction {
 
 #[derive(Debug)]
 struct CargoBay {
-    number_of_stacks: u32,
+    number_of_stacks: i32,
     stacks: Vec<Vec<String>>,
 }
 
 impl CargoBay {
-    fn new(number_of_stacks: u32) -> Self {
+    fn new(number_of_stacks: i32) -> Self {
         let mut stacks = Vec::new();
         for _ in 0..number_of_stacks {
             stacks.push(Vec::new());
@@ -44,8 +44,29 @@ impl CargoBay {
         }
     }
 
-    /*fn move_cargo(instruction: Instruction) {
-    }*/
+    fn stack_cargo(&mut self, cargo: Vec<&str>) {
+        for (i, stack) in self.stacks.iter_mut().enumerate() {
+            let current_cargo = cargo.get(i).unwrap();
+            if !current_cargo.to_string().is_empty() {
+                stack.push(current_cargo.to_string());
+            }
+        }
+    }
+
+    fn move_cargo(&mut self, instruction: Instruction) {
+        for _ in 0..instruction.quantity {
+            let current_cargo = self.stacks[(instruction.start - 1) as usize].pop().unwrap();
+            self.stacks[(instruction.end - 1) as usize].push(current_cargo);
+        }
+    }
+
+    fn get_top_cargo(&self) -> String {
+        let mut top_cargo = Vec::new();
+        self.stacks
+            .iter()
+            .for_each(|stack| top_cargo.push(stack[stack.len() - 1].clone()));
+        top_cargo.join(", ")
+    }
 }
 
 fn main() {
@@ -63,26 +84,36 @@ fn main() {
         .collect::<Vec<Vec<&str>>>();
     cargo_rows.pop();
 
+    let mut cargo_bay = CargoBay::new(i32::try_from(cargo_rows[0].len()).unwrap());
+
+    cargo_rows
+        .iter()
+        .rev()
+        .for_each(|row| cargo_bay.stack_cargo(row.to_vec()));
+
     let instuctions = instuctions
         .lines()
         .map(|instruction| {
             let instruction = instruction
-                .chars()
-                .filter(|character| character.is_numeric())
-                .map(|character| character.to_digit(10).unwrap())
-                .collect::<Vec<u32>>();
+                .split_whitespace()
+                .filter_map(|value| {
+                    let value = value.parse::<i32>();
+                    match value {
+                        Ok(value) => Some(value),
+                        Err(_) => None,
+                    }
+                })
+                .collect::<Vec<i32>>();
+
             Instruction::new(instruction[0], instruction[1], instruction[2])
         })
         .collect::<Vec<Instruction>>();
 
-    for instruction in instuctions {
-        dbg!(instruction);
-    }
+    instuctions
+        .into_iter()
+        .for_each(|instuction| cargo_bay.move_cargo(instuction));
 
-    for row in cargo_rows {
-        dbg!(row);
-    }
+    let top_cargo = cargo_bay.get_top_cargo();
 
-    let test = CargoBay::new(5);
-    dbg!(test);
+    println!("{top_cargo:?}");
 }
